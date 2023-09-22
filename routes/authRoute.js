43d562f1db44374e5c6aad1f8f283f3de3593a64
Ritik Tripathi <91249020+ritik1511@users.noreply.github.com>
2023-09-22 22:13:19 +0530

    final code
    online app for deployment

---
+++ routes/authRoute.js
@@ -0,0 +1,48 @@
+import express from "express";
+import { registerController, loginController, testController, forgotPasswordController, updateProfileController, getAllOrdersController, orderStatusController } from '../controllers/authController.js'
+import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
+
+//router object
+const router = express.Router()
+
+//routing
+//REGISTER || METHOD POST
+router.post('/register', registerController)
+//LOGIN || POST
+router.post('/login', loginController)
+
+
+//Forgot Password || POST
+router.post("/forgot-password", forgotPasswordController);
+
+
+//test route
+router.get('/test', requireSignIn, isAdmin, testController)
+
+//protected User route auth
+router.get("/user-auth", requireSignIn, (req, res) => {
+    res.status(200).send({ ok: true });
+});
+
+router.get("/admin-auth", requireSignIn, isAdmin, (req, res) => {
+    res.status(200).send({ ok: true });
+});
+
+//update profile
+router.put("/profile", requireSignIn, updateProfileController);
+
+//orders
+router.get("/orders", requireSignIn, getAllOrdersController);
+
+//all orders
+router.get("/all-orders", requireSignIn, isAdmin, getAllOrdersController);
+
+// order status update
+router.put(
+    "/order-status/:orderId",
+    requireSignIn,
+    isAdmin,
+    orderStatusController
+);
+
+export default router
---
+++ server.js
@@ -0,0 +1,48 @@
+import express from "express"  //after adding type as module in package json, we can use import insted of require(ES6 and later)
+import dotenv from 'dotenv' //env file is used to store environment variables so  that they cant be missused
+import morgan from "morgan"
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoute.js"
+import categoryRoutes from './routes/categoryRoutes.js'
+import productRoutes from './routes/productRoutes.js'
+import cors from 'cors'
+import path from 'path'
+
+//config env
+dotenv.config()
+
+//database config
+connectDB()
+
+//rest object
+const app = express()
+
+//middlewares
+app.use(cors());
+app.use(express.json())
+app.use(morgan('dev'))
+app.use(express.static(path.join(__dirname, './client/build')))
+
+//routes
+app.use('/api/v1/auth', authRoutes)
+app.use('/api/v1/category', categoryRoutes);
+app.use("/api/v1/product", productRoutes);
+// app.use("/api/v1/prediction");
+
+
+//rest api
+// app.get('/', (req, res) => {
+//     res.send("<h1>Welcome</h1>")
+// })
+app.use('*', function (req, res) {
+    res.sendFile(path.join(__dirname, './client/build/index.html'))
+})
+
+//PORT
+
+const PORT = process.env.PORT || 8080; //PORT variable resent in env file
+
+//run listen
+app.listen(PORT, () => {
+    console.log(`Server Running at ${PORT}`);
+})
